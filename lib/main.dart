6796cfb76db0c40b14cb31cc26aa0e965c4d5547
ahmedmohab96 <ahmdmhabhbyb@gmail.com>
2022-02-02 03:20:26 +0200

    https://github.com/ahmedmohab96/Yalla-Chat-.git

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:messageme_app/screens/chat_screen.dart';
+import 'package:messageme_app/screens/registration_scren.dart';
+import 'package:messageme_app/screens/signin_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'screens/welcome_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yalla Chat! app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: _auth.currentUser != null
+          ? ChatScreen.screenRoute
+          : WelcomeScreen.screenRoute,
+      routes: {
+        WelcomeScreen.screenRoute: (context) => WelcomeScreen(),
+        SignInScreen.screenRoute: (context) => SignInScreen(),
+        RegistrationScreen.screenRoute: (context) => RegistrationScreen(),
+        ChatScreen.screenRoute: (context) => ChatScreen(),
+      },
+    );
+  }
+}
